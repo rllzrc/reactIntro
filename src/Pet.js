0aefd87f0e88a1f9a8b12ec70a4fe92913982a26
Rex Crusade <rexcrusade@RexCrusade.local>
2019-12-29 15:12:09 -0500

    api search results updated, reformatted pet component

--- src/Pet.js
+++ src/Pet.js
@@ -11,7 +11,12 @@ import React from 'react';
 // * changed to:
 // * useful for debugging so stack trace to find that pet has the error
 // * default export for components is always the component
-export default function Pet ({ name, animal, breed }) {
+export default function Pet ({ name, animal, breed, media, location, id }) {
+
+  let hero = 'http://placecorgi.com/300/300';
+  if(media.length){
+    hero = media[0].small;
+  }
 
   // * using pure React without JSX
     // return React.createElement("div", {}, [
@@ -23,10 +28,24 @@ export default function Pet ({ name, animal, breed }) {
     // * using JSX syntax
     // * basically just the shorter version of the above
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location} `}</h2>
+      </div>
+    </a>
   );
 }
+
+
+// * used to be this one:
+// return (
+//   <div>
+//     <h1>{name}</h1>
+//     <h2>{animal}</h2>
+//     <h2>{breed}</h2>
+//   </div>
+// );
