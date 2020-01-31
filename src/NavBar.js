38fb64fc5d4044f0f2174d5e07ee1dc4226f820e
rllzrc <rellas.email@gmail.com>
2020-01-31 12:05:32 -0500

    add emotion library

---
+++ src/NavBar.js
@@ -0,0 +1,25 @@
+// * emotion is a library for including your styles inside of your component files and allowing you to harnees JS to easily script your CSS
+
+import React from 'react';
+import { Link } from '@react/router';
+import { css } from '@emotion/core';
+
+const NavBar = () => (
+  <header
+    css={css`
+      background-color: #333;
+      position: sticky,
+      top: 0;
+      z-index: 10;
+    `}
+  >
+  
+  <Link to='/'>Adopt Me!</Link>
+  <span arial-label='logo' role='img'>
+    🐶
+  </span>
+  </header>
+);
+
+// * emotion has other ways of interacting with it (generating components) but here we're using the new css prop way of doing it. 
+// * css prop allows us to use the css tagged template literal to write CSS.
